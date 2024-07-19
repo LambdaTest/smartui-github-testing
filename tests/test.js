f7ec1afdc6a5c414fcef61f788198e7216c28b7e
haiderLambdatest <haiderk@lambdatest.com>
2024-07-19 16:12:54 +0530

    GethealthieCheck-2

--- tests/test.js
+++ tests/test.js
@@ -37,7 +37,7 @@ async function searchTextOnGoogle() {
     accessKey: process.env.LT_ACCESS_KEY,
     name: "test session", // name of the test
     build: `${platform}${browserName}${version}`, // name of the build
-    "smartUI.build": "Github-baseline",
+    "smartUI.build": "Github-NonBaseline-1",
     "smartUI.project": "Gethealthie-integration-testing",
     github: {
       url: process.env.GITHUB_URL,
@@ -122,5 +122,4 @@ async function startTest(gridUrl, capabilities, name) {
     await driver.quit();
   }
 }
-
 searchTextOnGoogle();
