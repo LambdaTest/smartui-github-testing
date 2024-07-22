6d40406645f8dc62265b99754a2f24722ae74e58
haiderLambdatest <haiderk@lambdatest.com>
2024-07-22 19:01:26 +0530

    Gethealthie-1

--- tests/test.js
+++ tests/test.js
@@ -37,8 +37,8 @@ async function searchTextOnGoogle() {
     accessKey: process.env.LT_ACCESS_KEY,
     name: "test session", // name of the test
     build: `${platform}${browserName}${version}`, // name of the build
-    "smartUI.build": "Github-NonBaseline-2",
-    "smartUI.project": "Gethealthie-integration-testing",
+    "smartUI.build": "Github-Test-1",
+    "smartUI.project": "Gethealthie-integration",
     github: {
       url: process.env.GITHUB_URL,
     },
@@ -81,9 +81,9 @@ async function startTest(gridUrl, capabilities, name) {
 
     console.log("taking screenshot ...");
     setTimeout(() => {
-      driver.executeScript(`smartui.takeScreenshot=S-2`).then(() => {
+      driver.executeScript(`smartui.takeScreenshot=S-1`).then(() => {
         sleep(4000);
-        driver.executeScript(`smartui.fetchScreenshotStatus=S-2`).then(out => {
+        driver.executeScript(`smartui.fetchScreenshotStatus=S-1`).then(out => {
           console.log("response:", out);
         }).catch(err => {
           console.error("Error fetching screenshot status:", err);
