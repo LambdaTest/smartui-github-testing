e0dba768af622bf832a33ed7ebf934285b823742
haiderLambdatest <haiderk@lambdatest.com>
2024-07-22 19:21:47 +0530

    Gethealthie-6

--- tests/test.js
+++ tests/test.js
@@ -37,7 +37,7 @@ async function searchTextOnGoogle() {
     accessKey: process.env.LT_ACCESS_KEY,
     name: "test session", // name of the test
     build: `${platform}${browserName}${version}`, // name of the build
-    "smartUI.build": "Github-Test-3",
+    "smartUI.build": "Github-Test-4",
     "smartUI.project": "Gethealthie-integration",
     github: {
       url: process.env.GITHUB_URL,
