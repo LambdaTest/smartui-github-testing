2354663177c82ece91ed251c7d483f94208ad195
haiderLambdatest <haiderk@lambdatest.com>
2024-07-23 01:56:42 +0530

    Web-10

--- tests/test.js
+++ tests/test.js
@@ -23,9 +23,9 @@ async function searchTextOnGoogle() {
   console.log(keys);
   const parallelCount = keys[2] || 1;
   const tunnel = keys[3] || false;
-  // const platform = keys[4] || "Windows 10";
-  const platform = keys[4] || "bigsur";
-  const browserName = keys[5] || "safari";
+  const platform = keys[4] || "Windows 10";
+  // const platform = keys[4] || "bigsur";
+  const browserName = keys[5] || "chrome";
   const version = keys[6] || "latest";
 
   // Setup Input capabilities
