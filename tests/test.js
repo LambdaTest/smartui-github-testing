22868301dc02d0571b9e8e64caa4b507c991dd6b
haiderLambdatest <haiderk@lambdatest.com>
2024-07-23 18:51:20 +0530

    Web-48

--- tests/test.js
+++ tests/test.js
@@ -75,8 +75,8 @@ async function startTest(gridUrl, capabilities, name) {
     const duration = moment.duration(end_date.diff(start_date));
     console.log(`${caps.name} : Setup Time: ${duration.asSeconds()} seconds`);
 
-    const url = "https://www.lambdatest.com/";
-    // const url = "https://www.google.com/";
+    // const url = "https://www.lambdatest.com/";
+    const url = "https://www.google.com/";
     // const url = "https://www.samsung.com/";
 
     console.log(url);
