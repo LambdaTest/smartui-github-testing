58b138938857093806c5736b15e5ae7174978e7d
haiderLambdatest <haiderk@lambdatest.com>
2024-07-23 18:44:35 +0530

    Web-45

--- tests/test.js
+++ tests/test.js
@@ -39,7 +39,7 @@ async function searchTextOnGoogle() {
     accessKey: process.env.LT_ACCESS_KEY,
     name: "test session", // name of the test
     build: `${platform}${browserName}${version}`, // name of the build
-    // "smartUI.build": "smartui-99e5751623",
+    "smartUI.build": "smartui-8bc0d04e00",
     "smartUI.project": "MergeBaseline", 
     github: {
       url: process.env.GITHUB_URL,
@@ -76,13 +76,11 @@ async function startTest(gridUrl, capabilities, name) {
     console.log(`${caps.name} : Setup Time: ${duration.asSeconds()} seconds`);
 
     // const url = "https://www.lambdatest.com/";
-    const url = "https://www.google.com/";
-    // const url = "https://www.samsung.com/";
+    // const url = "https://www.google.com/";
+    const url = "https://www.samsung.com/";
 
     console.log(url);
     await driver.get(url);
-
-
 
     console.log("taking screenshot ...");
     setTimeout(() => {
