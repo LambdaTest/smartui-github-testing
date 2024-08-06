f9b42ed6514bdcaf36777b336fed2be021ecb09b
haiderLambdatest <haiderk@lambdatest.com>
2024-08-07 00:31:51 +0530

    07-08-24 | Commit-1

--- tests/test.js
+++ tests/test.js
@@ -14,9 +14,9 @@ const waitTime = 2; // 2 seconds
 // const GRID_HOST = process.env.GRID_HOST || "@hub.lambdatest.com/wd/hub"; //connect to lambdatest hub
 
 // const GRID_URL = process.env.GRID_URL || "GRID_URL";
-// const GRID_URL = "https://haiderk:V2mDZgIXHVEFxPfVu3cODSDpe9cZ4MT2Z1ZkDbp5uMJ8XL0nxh@stage-hub.lambdatestinternal.com/wd/hub";
+const GRID_URL = "https://haiderk:V2mDZgIXHVEFxPfVu3cODSDpe9cZ4MT2Z1ZkDbp5uMJ8XL0nxh@stage-hub.lambdatestinternal.com/wd/hub";
 // const GRID_URL = "https://smartui-internal:jNbKw8iwoDlqGuBfIBtd5F1Cnr4A0H07ifYNDGpylobjWyA2QX@stage-hub.lambdatestinternal.com/wd/hub";
-const GRID_URL = "https://haiderk:i7vF5r66IYgsgE9Hp5t6hZqd5PkQX021FgpWRC70mp0ShbFh1R@hub.lambdatest.com/wd/hub";
+// const GRID_URL = "https://haiderk:i7vF5r66IYgsgE9Hp5t6hZqd5PkQX021FgpWRC70mp0ShbFh1R@hub.lambdatest.com/wd/hub";
 
 async function searchTextOnGoogle() {
   const keys = process.argv;
@@ -39,8 +39,8 @@ async function searchTextOnGoogle() {
     accessKey: process.env.LT_ACCESS_KEY,
     name: "test session", // name of the test
     build: `${platform}${browserName}${version}`, // name of the build
-    "smartUI.build": "smartui-54fdc8d34b",
-    "smartUI.project": "Gethealthie-DOT-3410-1", 
+    "smartUI.build": "rachaelc/sc-118267/no-availability-1722894770",
+    "smartUI.project": "Gethealthie-Build_Issue", 
     github: {
       url: process.env.GITHUB_URL,
     },
@@ -76,8 +76,8 @@ async function startTest(gridUrl, capabilities, name) {
     console.log(`${caps.name} : Setup Time: ${duration.asSeconds()} seconds`);
 
     // const url = "https://www.lambdatest.com/";
-    // const url = "https://www.google.com/";
-    const url = "https://www.samsung.com/";
+    const url = "https://www.flipkart.com/";
+    // const url = "https://www.samsung.com/";
 
     console.log(url);
     await driver.get(url);
