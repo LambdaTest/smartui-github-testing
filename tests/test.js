5a3b7e0a64c2ddddeefcd3e2f13cd25aa12d5a76
haiderLambdatest <haiderk@lambdatest.com>
2024-08-07 17:32:10 +0530

    07-08-24 | Commit-25

--- tests/test.js
+++ tests/test.js
@@ -14,7 +14,7 @@ const waitTime = 2; // 2 seconds
 // const GRID_HOST = process.env.GRID_HOST || "@hub.lambdatest.com/wd/hub"; //connect to lambdatest hub
 
 // const GRID_URL = process.env.GRID_URL || "GRID_URL";
-const GRID_URL = "https://haiderk:V2mDZgIXHVEFxPfVu3cODSDpe9cZ4MT2Z1ZkDbp5uMJ8XL0nxh@stage-hub.lambdatestinternal.com/wd/hub";
+// const GRID_URL = "https://haiderk:V2mDZgIXHVEFxPfVu3cODSDpe9cZ4MT2Z1ZkDbp5uMJ8XL0nxh@stage-hub.lambdatestinternal.com/wd/hub";
 // const GRID_URL = "https://smartui-internal:jNbKw8iwoDlqGuBfIBtd5F1Cnr4A0H07ifYNDGpylobjWyA2QX@stage-hub.lambdatestinternal.com/wd/hub";
 // const GRID_URL = "https://haiderk:i7vF5r66IYgsgE9Hp5t6hZqd5PkQX021FgpWRC70mp0ShbFh1R@hub.lambdatest.com/wd/hub";
 
@@ -40,8 +40,8 @@ async function searchTextOnGoogle() {
     name: "test session", // name of the test
     build: `${platform}${browserName}${version}`, // name of the build
     "smartUI.build": "rachaelc/sc-118267/no-availability-1722894770/////",
-    // "smartUI.build": "Abc!@#$%^&*()_+[  ]\{}|;':<>?",
-    "smartUI.project": "Gethealthie-Build_Issue-2",    
+    "smartUI.build": "Abc!@#$%^&*()_+[  ]\{}|;':<>?",
+    // "smartUI.project": "Gethealthie-Build_Issue-2",    
     github: {
       url: process.env.GITHUB_URL,
     },
