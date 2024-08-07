126ef5d52d89d743033c6b40e07b7b142d2359c9
haiderLambdatest <haiderk@lambdatest.com>
2024-08-07 15:38:40 +0530

    07-08-24 | Commit-16

--- tests/test.js
+++ tests/test.js
@@ -39,8 +39,8 @@ async function searchTextOnGoogle() {
     accessKey: process.env.LT_ACCESS_KEY,
     name: "test session", // name of the test
     build: `${platform}${browserName}${version}`, // name of the build
-    "smartUI.build": "rachaelc/sc-118267/no-availability-1722894770//",
-    // "smartUI.build": "Abc!@#$%^&*()_+[]\{}|;':<>?",
+    // "smartUI.build": "rachaelc/sc-118267/no-availability-1722894770//",
+    "smartUI.build": "Abc!@#$%^&*()_+[]\{}|;':<>?",
     "smartUI.project": "Gethealthie-Build_Issue-1",    
     github: {
       url: process.env.GITHUB_URL,
